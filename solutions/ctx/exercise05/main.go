9f3c1e4140557989328bfabb563f71db208ba881
shixunzhao <shixunzhao@tencent.com>
2025-07-10 11:47:00 +0800

    feat:添加并发工作池实现

--- solutions/ctx/exercise05/main.go
+++ solutions/ctx/exercise05/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "context"
+import (
+	"context"
+	"sync"
+	"time"
+)
 
 /*
 第二题：带速率限制的并发工作池
@@ -29,27 +33,98 @@ Worker 从一个内部的 jobs channel 中获取任务。
 type Job func()
 
 // WorkerPool 工作池
+// 任务队列长度要足够大，否则submit会一直阻塞
 type WorkerPool struct {
-	workerCount   int
-	ratePerSecond int
-	ctx           context.Context
+	workerCount int
+	taskChan    chan Job
+	ctx         context.Context
+
+	// 令牌桶
+	ratePerSecond int        // 令牌生成速率
+	lastTimestamp time.Time  // 上次获取令牌的时间
+	currentTokens float64    // 令牌数
+	maxTokens     float64    // 最大令牌数
+	mu            sync.Mutex // 获取令牌锁
 }
+
+// taskChan的长度
+const (
+	taskChanCap = 100
+	maxTokens   = 100
+)
 
 // NewWorkerPool 工作池初始化函数
 func NewWorkerPool(ctx context.Context, workerCount int, ratePerSecond int) *WorkerPool {
+	// 初始化任务队列
+	taskChan := make(chan Job, taskChanCap)
+
+	var wg sync.WaitGroup
+	// 创建一个中间chan控制速率
+	rateChan := make(chan Job, taskChanCap)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+	}()
+
+	// 创建workerCount个goroutine监听任务队列
+
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			select {
+			case <-ctx.Done():
+				return
+			case task := <-taskChan:
+				task()
+			}
+		}()
+	}
+
+	// 负责关闭资源
+	go func() {
+		wg.Wait()
+		close(taskChan)
+		close(rateChan)
+	}()
+
 	return &WorkerPool{
 		workerCount:   workerCount,
 		ratePerSecond: ratePerSecond,
+		taskChan:      taskChan,
 		ctx:           ctx,
 	}
 }
 
 // Submit 向工作池提交一个任务。如果任务队列已满，此方法可以阻塞。
 func (w *WorkerPool) Submit(job Job) {
-
+	select {}
 }
 
 // Shutdown 优雅地关闭工作池。它应该停止接收新任务，并等待所有已在队列中和正在执行的任务完成后再返回。
 func (w *WorkerPool) Shutdown() {
 
+}
+
+// 判断是否允许获取
+func (w *WorkerPool) can() bool {
+	// 加锁
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	// 计算令牌数
+	currentTime := time.Now()
+	tokensGenerated := currentTime.Sub(w.lastTimestamp).Seconds() * float64(w.ratePerSecond)
+	w.currentTokens += tokensGenerated
+	if w.currentTokens >= w.maxTokens {
+		w.currentTokens = w.maxTokens
+	}
+
+	w.lastTimestamp = currentTime
+	if w.currentTokens < 1 {
+		return false
+	}
+
+	w.currentTokens--
+	return true
 }
